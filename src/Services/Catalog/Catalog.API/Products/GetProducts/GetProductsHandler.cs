74bb2374f47b4e438efad414b6e413ca7bb4a4ff
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-10-07 02:55:19 +0400

    implementing Pagination for getproducts

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,9 @@
-﻿namespace Catalog.API.Products.GetProducts;
+﻿using Marten.Pagination;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10)
+    : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsQueryHandler(IDocumentSession session)
@@ -8,7 +11,8 @@ internal class GetProductsQueryHandler(IDocumentSession session)
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken); ;
 
         return new GetProductsResult(products);
     }
