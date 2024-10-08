dffc4174f40689a0cc00ef0e77fd3a95817afd73
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-10-09 02:21:38 +0400

    developing basket repository using Marten IDocumentSession

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -3,7 +3,7 @@ namespace Basket.API.Basket.DeleteBasket;
 
 public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
 
-public record DeleteBasketResult(bool isSuccess);
+public record DeleteBasketResult(bool IsSuccess);
 
 public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
