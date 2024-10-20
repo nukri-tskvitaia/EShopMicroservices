46b2a786bf750d0151155511c915437cf489e371
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-10-21 02:06:17 +0400

    adding dependency injection in clean architecture layers and registering them in Ordering.API Program.cs

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    // Before building the application
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    // After building the application
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        return app;
+    }
+}
