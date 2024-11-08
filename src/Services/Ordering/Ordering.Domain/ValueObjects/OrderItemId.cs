b1033919b731d9efb90552f5f7e21dc24daa6309
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-11-09 01:56:54 +0400

    developing rich domain models for value objects

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
@@ -3,4 +3,17 @@
 public record OrderItemId
 {
     public Guid Value { get; }
+    private OrderItemId(Guid value) => Value = value;
+
+    public static OrderItemId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("OrderItemId can't be empty.");
+        }
+
+        return new OrderItemId(value);
+    }
 }
