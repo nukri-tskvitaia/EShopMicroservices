ca97c2ce91fb8d6798fad160f6ebb6b769792b30
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-11-09 01:33:53 +0400

    developing rich domain model for order entities

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -4,4 +4,20 @@ public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
+
+    // factory method
+    public static Customer Create(CustomerId customerId, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        var customer = new Customer
+        {
+            Id = customerId,
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+    }
 }
