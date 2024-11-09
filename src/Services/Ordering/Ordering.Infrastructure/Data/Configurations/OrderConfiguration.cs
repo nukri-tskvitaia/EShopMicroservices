dee0196389fa5a4675b680ec5358e4d0f9498419
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-11-09 18:07:56 +0400

    entity configurations part 2

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Enums;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System.Runtime.Loader;
 
 namespace Ordering.Infrastructure.Data.Configurations;
 
@@ -8,6 +11,47 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
     public void Configure(EntityTypeBuilder<Order> builder)
     {
-        throw new NotImplementedException();
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasConversion(orderId => orderId.Value,
+            dbId => OrderId.Of(dbId));
+        builder.HasOne<Customer>().WithMany().HasForeignKey(x => x.CustomerId).IsRequired();
+        builder.HasMany(x => x.OrderItems).WithOne().HasForeignKey(x => x.OrderId);
+        builder.ComplexProperty(x => x.OrderName, nameBuilder =>
+        {
+            nameBuilder.Property(n => n.Value).HasColumnName(nameof(Order.OrderName))
+                .HasMaxLength(100).IsRequired();
+        });
+        builder.ComplexProperty(x => x.ShippingAddress, addressBuilder =>
+        {
+            addressBuilder.Property(a => a.FirstName).HasMaxLength(50).IsRequired();
+            addressBuilder.Property(a => a.LastName).HasMaxLength(50).IsRequired();
+            addressBuilder.Property(a => a.EmailAddress).HasMaxLength(50);
+            addressBuilder.Property(a => a.AddressLine).HasMaxLength(180).IsRequired();
+            addressBuilder.Property(a => a.Country).HasMaxLength(50);
+            addressBuilder.Property(a => a.State).HasMaxLength(50);
+            addressBuilder.Property(a => a.ZipCode).HasMaxLength(5).IsRequired();
+        });
+        builder.ComplexProperty(x => x.BillingAddress, addressBuilder =>
+        {
+            addressBuilder.Property(a => a.FirstName).HasMaxLength(50).IsRequired();
+            addressBuilder.Property(a => a.LastName).HasMaxLength(50).IsRequired();
+            addressBuilder.Property(a => a.EmailAddress).HasMaxLength(50);
+            addressBuilder.Property(a => a.AddressLine).HasMaxLength(180).IsRequired();
+            addressBuilder.Property(a => a.Country).HasMaxLength(50);
+            addressBuilder.Property(a => a.State).HasMaxLength(50);
+            addressBuilder.Property(a => a.ZipCode).HasMaxLength(5).IsRequired();
+        });
+        builder.ComplexProperty(x => x.Payment, paymentBuilder =>
+        {
+            paymentBuilder.Property(p => p.CardName).HasMaxLength(50);
+            paymentBuilder.Property(p => p.CardNumber).HasMaxLength(24).IsRequired();
+            paymentBuilder.Property(p => p.Expiration).HasMaxLength(10);
+            paymentBuilder.Property(p => p.CVV).HasMaxLength(3);
+            paymentBuilder.Property(p => p.PaymentMethod);
+        });
+        builder.Property(x => x.Status).HasDefaultValue(OrderStatus.Draft)
+            .HasConversion(s => s.ToString(),
+            dbStatus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStatus));
+        builder.Property(x => x.TotalPrice);
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Infrastructure.Data.Configurations;
 
@@ -8,6 +9,11 @@ public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
 {
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
-        throw new NotImplementedException();
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasConversion(orderItemId => orderItemId.Value,
+            dbId => OrderItemId.Of(dbId));
+        builder.HasOne<Product>().WithMany().HasForeignKey(x => x.ProductId);
+        builder.Property(x => x.Quantity).IsRequired();
+        builder.Property(x => x.Price).IsRequired();
     }
 }
