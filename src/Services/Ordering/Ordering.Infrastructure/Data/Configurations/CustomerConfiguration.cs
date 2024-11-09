fd539f3ae5fac890241fb8b7222da87789295908
Nukri Tskvitaia <nukritskvitaia@gmail.com>
2024-11-09 17:36:17 +0400

    entity configurations part1

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasConversion(customerId => customerId.Value,
+            dbId => CustomerId.Of(dbId));
+        builder.Property(c => c.Name).IsRequired().HasMaxLength(100);
+        builder.Property(c => c.Email).IsRequired().HasMaxLength(255);
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
+}
